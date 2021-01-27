c0c33e410668cd374a214b89aff7ad7391467a79
martinodelamuerte <martinboujon@gmail.com>
2021-01-27 16:05:43 +0100

    Modif connectionString dbuser

--- API/index.js
+++ API/index.js
@@ -1,15 +1,15 @@
 const MongoClient = require('mongodb').MongoClient;
 const UtilisateursSchema = require('./models/Utilisateurs-model.js');
 
-const connectionString = 'mongodb+srv://Bojack:Bojack@projetgl.9eaqw.mongodb.net/<dbname>?retryWrites=true&w=majority'
+const connectionString = 'mongodb+srv://dbuser:dbuser@projetgl.9eaqw.mongodb.net/ProjetGl?retryWrites=true&w=majority'
 
 MongoClient.connect(connectionString, function(err, client) {
    if(err) {
         console.log('Error occurred while connecting to MongoDB Atlas...\n',err);
+        return 0;
    }
    console.log('Connected...');
-   const collection = client.db("ProjetGL").collection("ProjetGL");
+   const collection = client.db("ProjetGl").collection("ProjetGl");
    // perform actions on the collection object
    client.close();
-   // okdakor
 });
