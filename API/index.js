166094ca502de8e68762b4b7d0102141100593ba
vferront@gmail.com <Vf24081998>
2021-01-31 12:45:55 +0100

    install mongoose pour remplacer le module mongodb

--- API/index.js
+++ API/index.js
@@ -1,16 +1,23 @@
+const express = require('express');
+const bodyParser = require('body-parser');
 const MongoClient = require('mongodb').MongoClient;
 const UtilisateursSchema = require('./models/Utilisateurs-model.js');
-const express = require('express');
+
 
 const connectionString = 'mongodb+srv://dbuser:dbuser@projetgl.9eaqw.mongodb.net/ProjetGl?retryWrites=true&w=majority'
 
 const app = express();
 
-app.use(bodyParser.json()) // permet d afficher dans la console les posts data req.body
+app.use(bodyParser.json()) //permet d afficher dans la console les posts data req.body
 
 //routes
 app.get('/', (req,res) => {
   res.send("sweet home")
+})
+
+//login
+app.post('/login', (req,res) => {
+  console.log(req.body);
 })
 
 MongoClient.connect(connectionString, function(err, client) {
