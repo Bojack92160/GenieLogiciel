4bb9901ccd188d374f7f7472894c84da809aab84
vferront@gmail.com <Vf24081998>
2021-02-02 17:19:00 +0100

    Recherche de projet par titre, responsable, client, date ou id

--- API/path/Search.js
+++ API/path/Search.js
@@ -1,9 +1,72 @@
 var mongoose = require("mongoose");
-const UtilisateursSchema = require("./../models/Utilisateurs-model.js");
-const ClientsSchema = require("./../models/Clients-model.js");
-const ProjetsSchema = require("./../models/Projets-model.js");
+const UtilisateursSchema = require('./../models/Utilisateurs-model.js');
+const ClientsSchema = require('./../models/Clients-model.js');
+const ProjetsSchema = require('./../models/Projets-model.js');
+const NotificationsSchema = require('./../models/Notifications-model.js');
+const TachesSchema = require('./../models/Taches-model.js');
 
 module.exports = function (app) {
+
+  /** Recherche pour projet.
+   *  Champs possibles :
+   * @id //retourne une liste d'1 seul element max
+   * @titre
+   * @responsable
+   * @date //retourne les projets dont cette date est inclus entre dateDebutInit et dateFinInit
+   * Renvoi un liste (potentielle vide)
+   */
+  app.post("/Recherche/Projet", async (req, res) => {
+    if (!req.body.id && !req.body.titre && !req.body.responsable && !req.body.client && !req.body.date) {
+      res.json(await ProjetsSchema.find());
+      return;
+    }
+    let ResponseList = [];
+    if (req.body.id) {
+      let Response = await ProjetsSchema.findById(req.body.id);
+      if (Response) {
+        ResponseList.push(Response);
+        res.json(ResponseList);
+        return;
+      } else {
+        res.json([]);
+        return;
+      }
+    }
+
+    if (req.body.titre) {
+      res.json(await ProjetsSchema.find({"titre" : {$regex : ".*"+req.body.titre+".*"}}));
+      return;
+    } else if (req.body.responsable) {
+      res.json(await ProjetsSchema.find({"responsable" : req.body.responsable}));
+      return;
+    } else if (req.body.client) {
+      res.json(await ProjetsSchema.find({"client" : req.body.client}));
+      return;
+    } else if (req.body.date) {
+      ResponseList = await ProjetsSchema.find();
+      let FiltredResponseList = [];
+      try {
+        for (var i = 0; i < ResponseList.length; i++) {
+          let DateDebut = new Date(ResponseList[i].dateDebutInit);
+          let DateFin = new Date(ResponseList[i].dateFinInit);
+          let MyDate = new Date(req.body.date);
+          if (MyDate>=DateDebut && MyDate<=DateFin) {
+            FiltredResponseList.push(ResponseList[i]);
+          }
+        }
+        res.json(FiltredResponseList);
+        return;
+      } catch (e) {
+        res.json([]);
+        return;
+      }
+
+    }
+
+
+  });
+
+
   /** Recherche par Id Pure. Va rechercher dans toute les collections existantes
    *  requiert un champ :
    * @id
@@ -45,7 +108,16 @@ module.exports = function (app) {
         res.json(Response);
         return;
       }
-
+      Response = await NotificationsSchema.findById(req.body.id);
+      if (Response) {
+        res.json(Response);
+        return;
+      }
+      Response = await TachesSchema.findById(req.body.id);
+      if (Response) {
+        res.json(Response);
+        return;
+      }
       //TODO: RAJOUTER LES AUTRES COLLECTIONS
       res.json({
         erreur: "Aucun element ne correspond a cet id (" + req.body.id + ")",
