c461ecb7de4b6dbd126749903574a0bf24bd9206
vferront@gmail.com <Vf24081998>
2021-02-02 21:17:40 +0100

    Ajout des recherches par nom prenom email titre id des projets/taches + Utilisateur + Client

--- API/path/Search.js
+++ API/path/Search.js
@@ -62,7 +62,146 @@ module.exports = function (app) {
       }
 
     }
+  });
 
+  /** Recherche pour taches.
+   *  Champs possibles :
+   * @id //retourne une liste d'1 seul element max
+   * @titre
+   * @responsable
+   * @collaborateur
+   * @date //retourne les projets dont cette date est inclus entre dateDebutInit et dateFinInit
+   * Renvoi un liste (potentielle vide)
+   */
+  app.post("/Recherche/Tache", async (req, res) => {
+    if (!req.body.id && !req.body.titre && !req.body.responsable && !req.body.client && !req.body.date && !req.body.collaborateur) {
+      res.json(await TachesSchema.find());
+      return;
+    }
+    let ResponseList = [];
+    if (req.body.id) {
+      let Response = await TachesSchema.findById(req.body.id);
+      if (Response) {
+        ResponseList.push(Response);
+        res.json(ResponseList);
+        return;
+      } else {
+        res.json([]);
+        return;
+      }
+    }
+
+    if (req.body.titre) {
+      res.json(await TachesSchema.find({"titre" : {$regex : ".*"+req.body.titre+".*"}}));
+      return;
+    } else if (req.body.responsable) {
+      res.json(await TachesSchema.find({"responsable" : req.body.responsable}));
+      return;
+    } else if (req.body.responsable) {
+      res.json(await TachesSchema.find({"responsable" : req.body.responsable}));
+      return;
+    } else if (req.body.client) {
+      res.json(await TachesSchema.find({"collaborateur" : req.body.collaborateur}));
+      return;
+    } else if (req.body.date) {
+      ResponseList = await TachesSchema.find();
+      let FiltredResponseList = [];
+      try {
+        for (var i = 0; i < ResponseList.length; i++) {
+          let DateDebut = new Date(ResponseList[i].dateDebutInit);
+          let DateFin = new Date(ResponseList[i].dateFinInit);
+          let MyDate = new Date(req.body.date);
+          if (MyDate>=DateDebut && MyDate<=DateFin) {
+            FiltredResponseList.push(ResponseList[i]);
+          }
+        }
+        res.json(FiltredResponseList);
+        return;
+      } catch (e) {
+        res.json([]);
+        return;
+      }
+
+    }
+  });
+
+  /** Recherche pour utilisateur.
+   *  Champs possibles :
+   * @id //retourne une liste d'1 seul element max
+   * @nom
+   * @prenom
+   * @email
+   * Renvoi un liste (potentielle vide)
+   */
+  app.post("/Recherche/Utilisateur", async (req, res) => {
+    if (!req.body.id && !req.body.nom && !req.body.prenom && !req.body.email) {
+      res.json(await UtilisateursSchema.find());
+      return;
+    }
+    let ResponseList = [];
+    if (req.body.id) {
+      let Response = await UtilisateursSchema.findById(req.body.id);
+      if (Response) {
+        ResponseList.push(Response);
+        res.json(ResponseList);
+        return;
+      } else {
+        res.json([]);
+        return;
+      }
+    }
+
+    if (req.body.nom) {
+      res.json(await UtilisateursSchema.find({"nom" : {$regex : ".*"+req.body.nom+".*"}}));
+      return;
+    } else if (req.body.prenom) {
+      res.json(await UtilisateursSchema.find({"prenom" : {$regex : ".*"+req.body.prenom+".*"}}));
+      return;
+    } else if (req.body.email) {
+      res.json(await UtilisateursSchema.find({"email" : req.body.email}));
+      return;
+    }
+
+    res.json([]);
+    return;
+
+  });
+
+  /** Recherche pour utilisateur.
+   *  Champs possibles :
+   * @id //retourne une liste d'1 seul element max
+   * @nomEntreprise
+   * @email
+   * Renvoi un liste (potentielle vide)
+   */
+  app.post("/Recherche/Client", async (req, res) => {
+    if (!req.body.id && !req.body.nomEntreprise && !req.body.email) {
+      res.json(await ClientsSchema.find());
+      return;
+    }
+    let ResponseList = [];
+    if (req.body.id) {
+      let Response = await ClientsSchema.findById(req.body.id);
+      if (Response) {
+        ResponseList.push(Response);
+        res.json(ResponseList);
+        return;
+      } else {
+        res.json([]);
+        return;
+      }
+    }
+
+    if (req.body.nomEntreprise) {
+      res.json(await ClientsSchema.find({"nomEntreprise" : {$regex : ".*"+req.body.nomEntreprise+".*"}}));
+      return;
+    } else if (req.body.email) {
+      res.json(await ClientsSchema.find({"email" : req.body.email}));
+      return;
+    }
+
+    res.json([]);
+    return;
 
   });
 
