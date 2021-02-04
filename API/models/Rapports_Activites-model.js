bfb67139cb39a3e6683c83ed5a07305006909ae9
vferront@gmail.com <Vf24081998>
2021-02-04 16:26:09 +0100

    Creation du modele RapportsActivites

---
+++ API/models/Rapports_Activites-model.js
@@ -0,0 +1,18 @@
+const mongoose = require('mongoose');
+
+const Rapports_Activites = mongoose.Schema({
+  _idUtilisateur: String,
+  _idTache: String,
+  emailUtilisateur: String,
+  dateDeSaisie: Date,
+  periodeDebut: Date,
+  periodeFin: Date,
+  chargeEffectue: Number,
+  chargeRestante: Number, //=chargeEffectue * (1-avancementFinal)/avancementEffectué)
+  avancecementInitial: Number, //entre 0 et 1 (pourcentage)
+  avancementEffectué: Number,
+  avancementFinal: Number,
+  commentaire: String,
+});
+
+module.exports = mongoose.model('Utilisateurs', Rapports_Activites);
