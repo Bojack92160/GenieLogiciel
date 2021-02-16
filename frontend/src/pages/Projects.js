3a830a35a6a0541d84e37fc4e517e10ac5a8cdae
Najla Bezouich <najla.bezouich@u-psud.fr>
2021-02-16 23:20:42 +0100

    my-projects

--- frontend/src/pages/Projects.js
+++ frontend/src/pages/Projects.js
@@ -8,11 +8,22 @@ import TableHead from '@material-ui/core/TableHead';
 import TableRow from '@material-ui/core/TableRow';
 import Paper from '@material-ui/core/Paper';
 import { withStyles } from '@material-ui/core/styles';
+import PropTypes from 'prop-types';
+import LensRoundedIcon from '@material-ui/icons/LensRounded';
+import { Button } from '@material-ui/core';
 
 const useStyles = makeStyles({
   table: {
-    minWidth: "650px",
-    paddingTop: "20px"
+    height: 48,
+    margin: "auto",
+    width: "80%"
+    // padding: '0 10000px',
+  },
+  button: {
+    position: "fixed",
+    bottom: 20,
+    right: 20
+    // padding: '0 10000px',
   },
 });
 
@@ -28,13 +39,16 @@ const rows = [
   createData('Projet E', '11-01-2021--05-03-2021', 'v'),
   
 ];
-
+Projects.propTypes = {
+  classes: PropTypes.object.isRequired,
+};
 
 function Projects() {
   const classes = useStyles();
 
   return (
-    <TableContainer component={Paper}>
+    <div>
+       <TableContainer component={Paper}>
       <Table className={classes.table} aria-label="simple table">
         <TableHead>
           <TableRow>
@@ -51,14 +65,18 @@ function Projects() {
                 {row.titre}
               </TableCell>
               <TableCell >{row.periode}</TableCell>
-              <TableCell >{row.etat}</TableCell>
+              <TableCell ><LensRoundedIcon style={row.etat=='b' ? {color:"blue"} : row.etat=='v' ? {color:"red"} : null}/></TableCell>
               
              
             </TableRow>
           ))}
         </TableBody>
       </Table>
+      
     </TableContainer>
+    <Button variant="contained" className={classes.button}>+</Button>
+    </div>
+   
   );
 }
 
