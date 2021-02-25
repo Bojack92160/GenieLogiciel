19d80010fddafabe267236ae586b8be67e300410
Najla Bezouich <najla.bezouich@u-psud.fr>
2021-02-25 15:40:32 +0100

    recherche par id

--- frontend/src/components/CollabDisplaySelection.js
+++ frontend/src/components/CollabDisplaySelection.js
@@ -1,4 +1,4 @@
-import React from "react" 
+import {React,useState} from "react" 
 import { makeStyles } from '@material-ui/core/styles';
 
 const useStyles = makeStyles({
@@ -13,13 +13,33 @@ const useStyles = makeStyles({
 
 
 const CollabDisplaySelection = (props) => {
+  const [state, setState] = useState({
+    id: ""
+  });
+
+  const handleChangeId = (event) => {
+    setState({id: event.target.value})
+    const apiUrl = "http://localhost:3001/Recherche/Utilisateur";
+    var myHeaders = new Headers();
+    myHeaders.append("Content-Type", "application/json");
+    const req = { id: state.id };
+    var raw = JSON.stringify(req);
+    console.log(req);
+    var reqOptions = {
+      method: "POST",
+      headers: myHeaders,
+      body: raw
+    };
+    fetch(apiUrl, reqOptions)
+    .then((res) => console.log(res))
+  }
     const classes = useStyles();
 
     return (
         <div className={classes.groupedFields}>
             <div className={classes.searchField}>
             <input type="search" id="form1" className="form-control" placeholder="Id"
-            aria-label="Search" />
+            aria-label="Search" onChange={handleChangeId}/>
             </div>
             <div className={classes.searchField}>
             <input type="search" id="form1" className="form-control" placeholder="nom"
